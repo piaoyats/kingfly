96c3ac9cba465ba25237c0dbc2b641d9c89098d2
wangfei <scnbwf@yeah.net>
2014-05-05 08:27:54 +0800

    Update StudyExpression.scala

--- sparksql/src/main/scala/wfscala/StudyExpression.scala
+++ sparksql/src/main/scala/wfscala/StudyExpression.scala
@@ -1,24 +1,26 @@
 package wfscala
 
-import org.apache.spark.sql.catalyst.dsl
+import org.apache.spark.sql.catalyst.dsl.expressions._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkContext
 
 /**
  * Created by root on 14-5-3.
  */
 object StudyExpression {
   def main(args: Array[String]) {
-//    val sc = new SparkContext("local", "StudyExpression")
-//    val sqlContext = new SQLContext(sc)
-//    import sqlContext._
+//        val sc = new SparkContext("local", "StudyExpression")
+//        val sqlContext = new SQLContext(sc)
+//        import sqlContext._
 
     val s = 'aSymbol
     //输出true
     println( s == 'aSymbol)
     //输出true
     println( s == Symbol("aSymbol"))
-    import org.apache.spark.sql.catalyst.dsl._
-    val a = dsl.ExpressionConversions.DslSymbol('key)
-    val eprs = (a === 1)
+   // import org.apache.spark.sql.catalyst.dsl._
+//    val a = dsl.ExpressionConversions.DslSymbol('key)
+    val eprs = ('key === 1)
     println(eprs)
   }
 }
