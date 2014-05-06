da95b366c200a409908001b21a0acd00f4113744
wangfei <scnbwf@yeah.net>
2014-05-06 23:35:05 +0800

    add test unit

--- sparksql/src/main/scala/wfscala/StudyExpression.scala
+++ sparksql/src/main/scala/wfscala/StudyExpression.scala
@@ -9,17 +9,12 @@ import org.apache.spark.SparkContext
  */
 object StudyExpression {
   def main(args: Array[String]) {
-//        val sc = new SparkContext("local", "StudyExpression")
-//        val sqlContext = new SQLContext(sc)
-//        import sqlContext._
 
     val s = 'aSymbol
     //输出true
     println( s == 'aSymbol)
     //输出true
     println( s == Symbol("aSymbol"))
-   // import org.apache.spark.sql.catalyst.dsl._
-//    val a = dsl.ExpressionConversions.DslSymbol('key)
     val eprs = ('key === 1)
     println(eprs)
   }
