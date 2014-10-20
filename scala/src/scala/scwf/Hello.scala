e2aac23f4b23aa040daee8b9d099d58f8c6aff4c
scwf <wangfei1@huawei.com>
2014-10-20 16:35:42 +0800

    get the catch, but not know why

--- scala/src/scala/scwf/Hello.scala
+++ scala/src/scala/scwf/Hello.scala
@@ -13,18 +13,19 @@ object Hello {
       array.foldLeft(0)(_ + _)
     }
     import scala.util.{Success, Failure}
+    // call back, why onComplete not work!!!??? but onSuccess worked!
+    // get it, every time re import ExecutionContext.Implicits.global it will be ok
     f onComplete {
       case Success(a) => println(s"result is $a")
       case Failure(b) => println("An error has occured: " + b.getMessage)
     }
     //    Await.result(f, 1.minute)
-//        f.onSuccess {
-//          case a => println(a)
-//        }
+    f.onSuccess {
+          case a => println(a)
+        }
     //    f.onFailure {
     //      case a => println(a.getMessage)
     //    }
-    // call back, why onComplete not work!!!??? but onSuccess worked!
     println("end!")
   }
 }
