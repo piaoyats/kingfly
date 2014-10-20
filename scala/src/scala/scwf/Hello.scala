12a4e5a70bde8dc4809f820474dc6cd9ba293892
scwf <wangfei1@huawei.com>
2014-10-20 16:20:36 +0800

    add future but some wired thing about future, scala bug?

---
+++ scala/src/scala/scwf/Hello.scala
@@ -0,0 +1,30 @@
+package scala.scwf
+
+/**
+ * Created by w00228970 on 2014/10/20.
+ */
+object Hello {
+  def main(args: Array[String]) {
+    import scala.concurrent._
+    import ExecutionContext.Implicits.global
+    val array = 1 to 10
+    println("begin!")
+    val f: Future[Int] = future {
+      array.foldLeft(0)(_ + _)
+    }
+    import scala.util.{Success, Failure}
+    f onComplete {
+      case Success(a) => println(s"result is $a")
+      case Failure(b) => println("An error has occured: " + b.getMessage)
+    }
+    //    Await.result(f, 1.minute)
+//        f.onSuccess {
+//          case a => println(a)
+//        }
+    //    f.onFailure {
+    //      case a => println(a.getMessage)
+    //    }
+    // call back, why onComplete not work!!!??? but onSuccess worked!
+    println("end!")
+  }
+}
